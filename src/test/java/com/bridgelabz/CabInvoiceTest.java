9f0373deb2df7b7ee9eac420b46649d52a8e99f7
vinit <vinitkkulkarni@gmail.com>
2023-05-13 18:00:22 +0530

    Added:testcases for fare & minimum fare

--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -1,4 +1,23 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CabInvoiceTest {
+
+    @Test
+    public void givenDistanceAndTimeShouldReturnTotalFare(){
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
+        double result = cabInvoiceGenerator.calculateFare(3.0,3);
+        Assertions.assertEquals(33.0,result);
+    }
+
+    @Test
+    public void givenDistanceAndTimeShouldReturnMinimumFare(){
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
+        double result = cabInvoiceGenerator.calculateFare(0.2,1);
+        Assertions.assertEquals(5.0,result);
+    }
 }
