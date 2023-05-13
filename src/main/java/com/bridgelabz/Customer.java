80ad5ab87b9e3923fcf2a1251494d47b65ce7ae3
vinit <vinitkkulkarni@gmail.com>
2023-05-13 18:29:16 +0530

    Added:this class to hold customer data

---
+++ src/main/java/com/bridgelabz/Customer.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Customer {
+    int userId;
+    Ride rides[];
+
+
+    public Customer(int userId, Ride[] rides1) {
+        this.userId = userId;
+        this.rides = rides1;
+    }
+}
