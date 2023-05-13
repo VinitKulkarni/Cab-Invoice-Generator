34ce4414fa693f528351cbb0f14dd0fb19a29c7b
vinit <vinitkkulkarni@gmail.com>
2023-05-13 17:59:34 +0530

    Added:calculateFare() method

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,4 +1,13 @@
 package com.bridgelabz;
 
 public class CabInvoiceGenerator {
+
+    static final double COST_PER_KILOMETER = 10;
+    static final double COST_PER_MINUTE = 1;
+    static final double MIN_FARE = 5;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        return totalFare < MIN_FARE ? MIN_FARE : totalFare;
+    }
 }
