3f8b5101ef0030e0362180e548dd881b55f7e340
vinit <vinitkkulkarni@gmail.com>
2023-05-13 19:30:39 +0530

    Refactored:all methods to match for different ride category

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -4,30 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CabInvoiceGenerator {
-
-    static final double COST_PER_KILOMETER = 10;
-    static final double COST_PER_MINUTE = 1;
-    static final double MIN_FARE = 5;
     static List<Customer> rideRepo = new ArrayList<>();
 
-    public double calculateFare(double distance, int time) {
-        double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
-        return totalFare < MIN_FARE ? MIN_FARE : totalFare;
+    public double calculateFare(double distance, int time, RideCategory rideCategory) {
+        double totalFare = distance * rideCategory.getMIN_FARE() + time * rideCategory.getCOST_PER_KILOMETER();
+        return totalFare < rideCategory.getMIN_FARE() ? rideCategory.getMIN_FARE() : totalFare;
     }
 
     public double calculateFare(Ride[] rides) {
         double aggrigateFare = 0.0;
         for(Ride ride: rides){
-            aggrigateFare = aggrigateFare + calculateFare(ride.getDistance(),ride.getTime());
+            aggrigateFare = aggrigateFare + calculateFare(ride.getDistance(),ride.getTime(),ride.getRideCategory());
         }
         return aggrigateFare;
     }
 
     public Invoice generateInvoice(Ride[] rides) {
+        RideCategory rideCategoryValue = null;
+        for(Ride ride: rides){
+            rideCategoryValue = ride.getRideCategory();
+        }
         double totalFare = calculateFare(rides);
         int numberOfRides = rides.length;
         double average = totalFare / numberOfRides;
-        return new Invoice(totalFare,numberOfRides,average);
+        return new Invoice(totalFare,numberOfRides,average,rideCategoryValue);
     }
 
     public Invoice generateInvoiceByUserId(int userId) {
