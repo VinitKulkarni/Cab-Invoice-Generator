99297976daf085ddaa4d338047a1f5816c9fe456
vinit <vinitkkulkarni@gmail.com>
2023-05-13 19:31:57 +0530

    Added: ride category

--- src/main/java/com/bridgelabz/Invoice.java
+++ src/main/java/com/bridgelabz/Invoice.java
@@ -6,12 +6,46 @@ public class Invoice {
     private int numberOfRides;
     private double average;
     private double totalFare;
+    RideCategory rideCategory;
 
 
-    public Invoice(double totalFare, int numberOfRides, double average) {
+    public Invoice(double totalFare, int numberOfRides, double average, RideCategory rideCategory) {
         this.totalFare = totalFare;
         this.numberOfRides = numberOfRides;
         this.average = average;
+        this.rideCategory = rideCategory;
+    }
+
+    public int getNumberOfRides() {
+        return numberOfRides;
+    }
+
+    public void setNumberOfRides(int numberOfRides) {
+        this.numberOfRides = numberOfRides;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+    public void setAverage(double average) {
+        this.average = average;
+    }
+
+    public double getTotalFare() {
+        return totalFare;
+    }
+
+    public void setTotalFare(double totalFare) {
+        this.totalFare = totalFare;
+    }
+
+    public RideCategory getRideCategory() {
+        return rideCategory;
+    }
+
+    public void setRideCategory(RideCategory rideCategory) {
+        this.rideCategory = rideCategory;
     }
 
     @Override
@@ -19,11 +53,11 @@ public class Invoice {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Invoice invoice = (Invoice) o;
-        return numberOfRides == invoice.numberOfRides && Double.compare(invoice.average, average) == 0 && Double.compare(invoice.totalFare, totalFare) == 0;
+        return numberOfRides == invoice.numberOfRides && Double.compare(invoice.average, average) == 0 && Double.compare(invoice.totalFare, totalFare) == 0 && rideCategory == invoice.rideCategory;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(numberOfRides, average, totalFare);
+        return Objects.hash(numberOfRides, average, totalFare, rideCategory);
     }
 }
